1caa26fa6a9ddc9aa8dfaed2f689f46af72374ac
dominicjunik <111475701+dominicjunik@users.noreply.github.com>
2023-08-01 16:49:03 -0600

    shadows on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -202,7 +202,7 @@ export default function Home() {
 
    
     return (
-        <div className="bg-parchment w-96 ">
+        <div className="bg-parchment w-96 shadow-pop-out">
             <h1>D&D API CHARACTER CREATOR</h1>
 
             {step.race && step.class && step.abilityScore && step.alignment && step.background ? null : (step.race ? <Link to={journey()}> <div>NEXT STEP</div></Link> : <Link to="/races"> <div>START</div></Link>)}
